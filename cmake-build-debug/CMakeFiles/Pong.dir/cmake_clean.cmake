d76480a7c2782d8136d48d2226d99af163ebd637
michael-pruglo <michael.pruglo.work@gmail.com>
2020-06-12 15:53:31 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Pong.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pong.dir/main.cpp.obj"
+  "Pong.exe"
+  "Pong.exe.manifest"
+  "Pong.pdb"
+  "libPong.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
