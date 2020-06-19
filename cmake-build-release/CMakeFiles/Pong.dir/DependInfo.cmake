cbd0dcd23a3e019d3b2fe83dbe318e30eac6bb70
michael-pruglo <michael.pruglo.work@gmail.com>
2020-06-19 10:58:00 +0300

    added release configuration

---
+++ cmake-build-release/CMakeFiles/Pong.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/mickl/ClionProjects/Pong/src/controller/GameManager.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/controller/GameManager.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/main.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/main.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/model/Ball.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/model/Ball.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/model/Pong.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/model/Pong.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/model/PongBot.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/model/PongBot.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/model/PongPlayer.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/model/PongPlayer.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/utility/CollisionDetector.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/utility/CollisionDetector.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/utility/Random.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/utility/Random.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/view/GameWindow.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/view/GameWindow.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/view/RectSprite.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/view/RectSprite.cpp.obj"
+  "C:/Users/mickl/ClionProjects/Pong/src/view/TextSprite.cpp" "C:/Users/mickl/ClionProjects/Pong/cmake-build-release/CMakeFiles/Pong.dir/src/view/TextSprite.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SDL_MAIN_HANDLED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/msys64/mingw64/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release/CMakeFiles/Pong.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pong.dir/src/controller/GameManager.cpp.obj"
+  "CMakeFiles/Pong.dir/src/main.cpp.obj"
+  "CMakeFiles/Pong.dir/src/model/Ball.cpp.obj"
+  "CMakeFiles/Pong.dir/src/model/Pong.cpp.obj"
+  "CMakeFiles/Pong.dir/src/model/PongBot.cpp.obj"
+  "CMakeFiles/Pong.dir/src/model/PongPlayer.cpp.obj"
+  "CMakeFiles/Pong.dir/src/utility/CollisionDetector.cpp.obj"
+  "CMakeFiles/Pong.dir/src/utility/Random.cpp.obj"
+  "CMakeFiles/Pong.dir/src/view/GameWindow.cpp.obj"
+  "CMakeFiles/Pong.dir/src/view/RectSprite.cpp.obj"
+  "CMakeFiles/Pong.dir/src/view/TextSprite.cpp.obj"
+  "Pong.exe"
+  "Pong.exe.manifest"
+  "Pong.pdb"
+  "libPong.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
